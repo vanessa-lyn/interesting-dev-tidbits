5c90d8c77d7a761a0fe8f8c04e8e160479d92011
Vanessa G <vgarcia@sapient.com>
2014-07-21 18:14:59 -0700

    events

--- angular-tutorial/maincontroller.js
+++ angular-tutorial/maincontroller.js
@@ -47,5 +47,16 @@ app.controller('MainController', function($scope){
             live: true
         }
     ];
+    $scope.newPerson = null;
+    $scope.addNew = function() {
+        if ($scope.newPerson != null && $scope.newPerson != '') {
+            $scope.people.push({
+                id: $scope.people.length,
+                name: $scope.newPerson,
+                live: true,
+                music: []
+            });
+        }
+    }
 
 });
